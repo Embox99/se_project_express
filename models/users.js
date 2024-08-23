2c14682941db9100595a91599422bcf7272a4446
Eduard Vilensky <embox53@gmail.com>
2024-08-23 16:53:11 +0300

    fix

--- models/users.js
+++ models/users.js
@@ -12,33 +12,38 @@ const userSchema = new mongoose.Schema({
         return validator.isURL(value);
       },
       message: "You must enter a valid URL",
-    },},
-    email: {
-      type: String,
-      required: [true, "the email field is required"],
-      validate: {
-        validator(value) {
-          return validator.isEmail(value);
-        },
+    },
+  },
+  email: {
+    type: String,
+    required: [true, "the email field is required"],
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
       },
     },
-    password: { type: String, required: [true, "the password is required"], select: false, },
+  },
+  password: {
+    type: String,
+    required: [true, "the password is required"],
+    select: false,
+  },
 });
 
-userSchema.statics.findUserByCredentials() = function(email, password){
-  return this.findOne({email}).select('+password')
-  .then((user)=>{
-    if(!user){
-      return Promise.reject(new Error("wrong email or password"))
-    }
-    return bcrypt.compare(password, user.password)
-    .then((matched)=>{
-      if(!matched){
-        return Promise.reject(new Error("wrong email or password"))
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("wrong email or password"));
       }
-      return user;
-    })
-  })
-}
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("wrong email or password"));
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
